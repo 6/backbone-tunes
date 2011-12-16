529000711a15d5ee53e68be57f8f366bbccaed1b
Peter Graham <peterghm@gmail.com>
2011-12-16 18:58:08 -0500

    woops - actually start from scratch

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,23 +1,3 @@
 (function($) {
-  
-  window.Album = Backbone.Model.extend({});
-  
-  window.AlbumView = Backbone.View.extend({
-    tagName: 'li',
-    className: 'album',
-    
-    initialize: function() {
-      _.bindAll(this, 'render');
-      this.model.bind('change', this.render);
-      
-      this.template = _.template($("#album-template").html());
-    },
-    
-    render: function() {
-      var renderedContent = this.template(this.model.toJSON());
-      $(this.el).html(renderedContent);
-      return this;
-    }
-    
-  });
+
 })(jQuery);
