5871697eedbac2c1b8caae8f03444eda37d82212
Peter Graham <peterghm@gmail.com>
2011-12-16 19:18:51 -0500

    chapter 1, models and views

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,3 +1,20 @@
 (function($) {
 
+  // the most basic model
+  window.Album = Backbone.Model.extend({});
+
+  window.AlbumView = Backbone.View.extend({
+    
+    initialize: function() {
+      // template to associate with this view
+      this.template = _.template($('#album-template').html());
+    }
+    
+    ,render: function() {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent); // el is wrapper element of this view
+      return this; // return this so we can chain other calls
+    }
+  });
+
 })(jQuery);
